c10e8a618154d9fecd728c1d03cf9d22de99634a
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:12:30 -0700

    feat: add web storage module

---
+++ engine/web-lib/storage.js
@@ -0,0 +1,1 @@
+export const storage={get(k,d=null){try{return JSON.parse(localStorage.getItem(k))??d}catch{return d}},set(k,v){localStorage.setItem(k,JSON.stringify(v));return v},remove:k=>localStorage.removeItem(k)};
