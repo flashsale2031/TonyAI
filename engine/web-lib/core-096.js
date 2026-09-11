afa0278ac0ffb28e68604cf5296c0ee73f796199
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:35:58 -0700

    Add web library core module 096

---
+++ engine/web-lib/core-096.js
@@ -0,0 +1,2 @@
+export const core096={canvas:(w=300,h=150)=>Object.assign(document.createElement('canvas'),{width:w,height:h}),ctx:(c)=>c.getContext('2d')};
+if(typeof window!=='undefined')window.TONYCore096=core096;
