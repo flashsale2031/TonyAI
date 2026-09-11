594948a0b36af6f3c60422a90e6f9be3b363e348
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:30:18 -0700

    Add web library core module 024

---
+++ engine/web-lib/core-024.js
@@ -0,0 +1,2 @@
+export const core024={includes:(v,x)=>String(v??'').includes(String(x)),starts:(v,x)=>String(v??'').startsWith(String(x))};
+if(typeof window!=='undefined')window.TONYCore024=core024;
