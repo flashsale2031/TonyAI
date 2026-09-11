0e0da9eacef49114578eb43cf9af17f212208e01
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:33:04 -0700

    Add web library core module 058

---
+++ engine/web-lib/core-058.js
@@ -0,0 +1,2 @@
+export const core058={width:(el)=>el?.getBoundingClientRect?.().width??0,height:(el)=>el?.getBoundingClientRect?.().height??0};
+if(typeof window!=='undefined')window.TONYCore058=core058;
