65afb5913284f191eba2bdf14bc3633982c68a4c
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:32:38 -0700

    Add web library core module 053

---
+++ engine/web-lib/core-053.js
@@ -0,0 +1,2 @@
+export const core053={min:(v)=>Math.min(...Array.from(v??[]).map(Number)),max:(v)=>Math.max(...Array.from(v??[]).map(Number))};
+if(typeof window!=='undefined')window.TONYCore053=core053;
