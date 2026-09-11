7390f9c78fc77505f32d7951e3890a5dc8b63f3c
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:33:35 -0700

    Add web library core module 065

---
+++ engine/web-lib/core-065.js
@@ -0,0 +1,2 @@
+export const core065={find:(v,p)=>Array.from(v??[]).find(p),some:(v,p)=>Array.from(v??[]).some(p),every:(v,p)=>Array.from(v??[]).every(p)};
+if(typeof window!=='undefined')window.TONYCore065=core065;
