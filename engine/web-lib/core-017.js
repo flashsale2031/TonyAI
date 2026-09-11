f8c3507b2945db37ac6264d64cdbcb09454fe5f1
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:23:39 -0700

    Add web library core module 017

---
+++ engine/web-lib/core-017.js
@@ -0,0 +1,2 @@
+export const core017={keys(value){return Object.keys(value??{});},values(value){return Object.values(value??{});},entries(value){return Object.entries(value??{});},fromEntries(value){return Object.fromEntries(value??[]);}};
+if(typeof window!=='undefined')window.TONYCore017=core017;
