4d6d3320917af949472dbd5fcb2f0b8388b0a4c6
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:33:21 -0700

    Add web library core module 062

---
+++ engine/web-lib/core-062.js
@@ -0,0 +1,2 @@
+export const core062={floor:(v)=>Math.floor(Number(v)||0),ceil:(v)=>Math.ceil(Number(v)||0),sign:(v)=>Math.sign(Number(v)||0)};
+if(typeof window!=='undefined')window.TONYCore062=core062;
