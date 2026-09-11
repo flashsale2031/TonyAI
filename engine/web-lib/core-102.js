1c7d53593d0a4fcd2793e94dbbab1180d0461ee5
flashsale2031 <flashsale2031@gmail.com>
2026-09-11 13:36:25 -0700

    Add web library core module 102

---
+++ engine/web-lib/core-102.js
@@ -0,0 +1,2 @@
+export const core102={cssRule:(css)=>{const s=document.createElement('style');s.textContent=css;document.head.append(s);return s}};
+if(typeof window!=='undefined')window.TONYCore102=core102;
